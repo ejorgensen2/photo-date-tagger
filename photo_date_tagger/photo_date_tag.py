643c82a1f33fd57d0677c3ec6a8bfba56b3adaea
EJ <8682176+ejorgensen2@users.noreply.github.com>
2024-05-01 11:22:08 -0400

    Add image plotting and standardize augmentations

--- photo_date_tagger/photo_date_tag.py
+++ photo_date_tagger/photo_date_tag.py
@@ -7,8 +7,10 @@ import os
 from os import listdir
 
 import easyocr
+import itertools
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
+from matplotlib import pyplot as plt
 
 IMG_EXTENSIONS = (".png", ".jpg", ".jpeg", ".tiff", ".bmp", ".gif")
 ALLOWED_CHARS = {f"{i}" for i in range(10)} | {"'", "-", " "}
@@ -26,31 +28,45 @@ def extract_imprinted_date(photo_path: str) -> str | None:
         str | None: Date string imprinted on photo or None if not found or errored.
     """
     try:
-        # Extract text and confidence level from photo, photo cropped to corner, and zoomed cropped photo
+        # Extract text and confidence level from photo and several augmented forms of the photo.
         print(f"Checking photo at {photo_path}")
-        img = np.array(Image.open(photo_path))
-        extracted = ocr_reader.readtext(img, allowlist=ALLOWED_CHARS, width_ths=0.7)
-        if img.shape[0] < img.shape[1]:
-            img = img[img.shape[0] // 2 :, img.shape[1] // 2 :, :]
-        else:
-            img = img[img.shape[0] // 2 :, 0 : img.shape[1] // 2, :]
-        extracted_cropped = ocr_reader.readtext(img, allowlist=ALLOWED_CHARS, width_ths=0.7)
-        extracted_zoomed = ocr_reader.readtext(img[::2, ::2, :], allowlist=ALLOWED_CHARS, width_ths=0.7)
+        img_forms = {"orig": np.array(Image.open(photo_path))}
+        
+        M, N, _ = img_forms['orig'].shape
+        img_forms["downsamp"] = img_forms["orig"][::2, ::2, :]
 
-        # Throw away bounding boxes
-        extracted = [txt[-2:] for txt in extracted]
-        extracted_cropped = [txt[-2:] for txt in extracted_cropped]
-        extracted_zoomed = [txt[-2:] for txt in extracted_zoomed]
-        print(f"Text found in photo: {extracted}")
-        print(f"Text found in cropped photo: {extracted_cropped}")
-        print(f"Text found in zoomed photo: {extracted_zoomed}")
+        img_forms["50perc_crop"] = img_forms["orig"][M//2:, N//2:, :] if M < N else img_forms["orig"][M//2:, 0:N//2, :]
+        img_forms["50perc_crop_downsamp"] = img_forms["50perc_crop"][::2, ::2, :]
+
+        img_forms["33perc_crop"] = img_forms["orig"][2*M//3:, 2*N//3:, :] if M < N else img_forms["orig"][2*M//3:, 0:N//3, :]
+        img_forms["33perc_crop_downsamp"] = img_forms["33perc_crop"][::2, ::2, :]
+
+        # Plot image manipulations and their respective color histograms.
+        _, ax = plt.subplots(2, len(img_forms), figsize=(len(img_forms)*3.5,7))
+        for i, key in enumerate(img_forms):
+            # Plot image
+            ax[0,i].imshow(img_forms[key])
+            ax[0,i].set_title(f"{key} - {img_forms[key].shape}")
+            ax[0,i].set_xticks([])
+            ax[0,i].set_yticks([])
+
+            # Plot channel histogram
+            for ch, col in [(0, 'r'), (1, 'g'), (2, 'b')]:
+                hist, bins = np.histogram(img_forms[key][ch].flatten(), bins=256, range=(0,256))
+                ax[1,i].plot(bins[:-1], hist, color=col, lw=0.5)
+        plt.tight_layout()
+        plt.show()
+
+        # Run OCR to extract text
+        extracted_text_w_boxes = {k: ocr_reader.readtext(v, allowlist=ALLOWED_CHARS, width_ths=0.7) for k,v in img_forms.items()}
+        extracted_text = {k: [txt[-2:] for txt in v] for k,v in extracted_text_w_boxes.items()}
 
         # Grab the highest confidence prediction out of the predictions that have valid length
-        all_preds = [pred for pred in extracted + extracted_cropped + extracted_zoomed if 4 <= len(pred[0]) <= 12]
+        all_preds = [x for x in sum(extracted_text.values(), []) if 4 <= len(x[0]) <= 12]
         if len(all_preds):
-            extracted_text = max(all_preds, key=lambda x: x[1])
-            print(extracted_text)
-            return extracted_text[0]
+            highest_conf_text = max(all_preds, key=lambda x: x[1])
+            print(highest_conf_text)
+            return highest_conf_text[0]
 
         raise Exception
 
@@ -60,7 +76,7 @@ def extract_imprinted_date(photo_path: str) -> str | None:
 
 
 def parse_file_or_dir() -> list:
-    """Return list of files in directory argument or filepath provided.
+    """Return list of files in directory argument or filepath provided via Command Line.
 
     Raises:
         ValueError: If both file and directory arguments are provided.
@@ -78,12 +94,12 @@ def parse_file_or_dir() -> list:
     elif args.file and not args.dir:
         print(f"Checking for date in photo file {args.file}")
         file = args.file if os.path.isabs(args.file) else os.path.join(os.path.dirname(__file__), args.file)
-        return [file] if os.path.isfile(file) else []
+        return [os.path.abspath(file)] if os.path.isfile(file) else []
     else:
         dir = args.dir if args.dir else "../test_photos/"
         dir = dir if os.path.isabs(dir) else os.path.join(os.path.dirname(__file__), dir)
         print(f"Checking for date in photo directory {dir}")
-        return [os.path.join(dir, f) for f in listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+        return [os.path.abspath(os.path.join(dir, f)) for f in listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 
 
 def check_photo_dates() -> int:
@@ -101,7 +117,7 @@ def check_photo_dates() -> int:
         date_str = extract_imprinted_date(file)
         print(file, date_str)
 
-    print(f"Finished function <check_photo_dates> within {os.path.realpath(__file__)}")
+    print(f"\nFinished function <check_photo_dates> within {os.path.realpath(__file__)}")
     return 0
 
 
